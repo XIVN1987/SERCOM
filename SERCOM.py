76b3876e85bc00dd657ad4c56d831d5c86047019
XIVN1987 <XIVN1987@163.com>
2024-06-15 02:30:01 +0800

    add UDP transfer function

--- SERCOM.py
+++ SERCOM.py
@@ -1,8 +1,13 @@
 #! python3
 import os
 import sys
+import queue
+import socket
 import datetime
+import threading
+import collections
 import configparser
+import time
 
 from PyQt5 import QtCore, QtGui, uic
 from PyQt5.QtCore import pyqtSlot, Qt
@@ -29,6 +34,7 @@ class SERCOM(QWidget):
 
         for port, desc, hwid in comports():
             self.cmbPort.addItem(f'{port} ({desc[:desc.index("(")]})')
+        self.cmbPort.addItems(['UDP Client', 'UDP Server'])
 
         self.ser = Serial()
 
@@ -36,15 +42,22 @@ class SERCOM(QWidget):
 
         self.initQwtPlot()
 
+        self.closed = True      # 通信接口是否关闭
+        self.exited = False     # 应用程序是否退出
+
         self.rcvbuff = b''
         self.rcvfile = None
 
-        self.tmrSer = QtCore.QTimer()
-        self.tmrSer.setInterval(10)
-        self.tmrSer.timeout.connect(self.on_tmrSer_timeout)
-        self.tmrSer.start()
+        self.rcvQueue = queue.Queue()
 
-        self.tmrSer_Cnt = 0
+        threading.Thread(target=self.thread_sock_rcv).start()    # call sock.recvfrom() in on_tmrRecv_timeout will hang GUI
+
+        self.tmrRecv = QtCore.QTimer()
+        self.tmrRecv.setInterval(10)
+        self.tmrRecv.timeout.connect(self.on_tmrRecv_timeout)
+        self.tmrRecv.start()
+
+        self.tmrRecv_Cnt = 0
         self.AutoInterval = 0   # 自动发送时间间隔，单位 10ms
     
     def initSetting(self):
@@ -58,6 +71,10 @@ class SERCOM(QWidget):
             self.conf.add_section('serial')
             self.conf.set('serial', 'port', 'COM0')
             self.conf.set('serial', 'baud', '57600')
+
+            self.conf.add_section('network')
+            self.conf.set('network', 'ip', '[]')
+            self.conf.set('network', 'port', '9000')
 
             self.conf.add_section('encode')
             self.conf.set('encode', 'input', 'ASCII')
@@ -86,7 +103,11 @@ class SERCOM(QWidget):
         index = self.cmbPort.findText(self.conf.get('serial', 'port'))
         self.cmbPort.setCurrentIndex(index if index != -1 else 0)
         self.cmbBaud.setCurrentIndex(self.cmbBaud.findText(self.conf.get('serial', 'baud')))
-    
+
+        ips = eval(self.conf.get('network', 'ip')) + socket.gethostbyname_ex(socket.gethostname())[2]
+        self.cmbIP.addItems(list(collections.OrderedDict.fromkeys(ips[:20])))   # 保留顺序去重
+        self.linPort.setText(self.conf.get('network', 'port'))
+
     def initQwtPlot(self):
         self.PlotData  = [[0]*self.N_POINT for i in range(self.N_CURVE)]
         self.PlotPoint = [[QtCore.QPointF(j, 0) for j in range(self.N_POINT)] for i in range(self.N_CURVE)]
@@ -95,66 +116,138 @@ class SERCOM(QWidget):
 
         self.ChartView = QChartView(self.PlotChart)
         self.ChartView.setVisible(False)
-        self.vLayout0.insertWidget(0, self.ChartView)
+        self.vLayout.insertWidget(0, self.ChartView)
         
         self.PlotCurve = [QLineSeries() for i in range(self.N_CURVE)]
 
     @pyqtSlot()
     def on_btnOpen_clicked(self):
-        if not self.ser.is_open:
+        port = self.cmbPort.currentText()
+        if self.closed:
             try:
-                self.ser.timeout = 1
-                self.ser.xonxoff = 0
-                self.ser.port = self.cmbPort.currentText().split()[0]
-                self.ser.parity = self.cmbChek.currentText()[0]
-                self.ser.baudrate = int(self.cmbBaud.currentText())
-                self.ser.bytesize = int(self.cmbData.currentText())
-                self.ser.stopbits = int(self.cmbStop.currentText())
-                self.ser.open()
+                if port == 'UDP Server':
+                    self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    self.sock.bind((self.cmbIP.currentText(), int(self.linPort.text())))
+                elif port == 'UDP Client':
+                    self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                else:
+                    self.ser.timeout = 1
+                    self.ser.xonxoff = 0
+                    self.ser.port = self.cmbPort.currentText().split()[0]
+                    self.ser.parity = self.cmbChek.currentText()[0]
+                    self.ser.baudrate = int(self.cmbBaud.currentText())
+                    self.ser.bytesize = int(self.cmbData.currentText())
+                    self.ser.stopbits = int(self.cmbStop.currentText())
+                    self.ser.open()
 
                 if self.chkSave.isChecked():
                     self.rcvfile = open(os.path.join(self.savedir, datetime.datetime.now().strftime("rcv_%y%m%d%H%M%S.txt")), 'w')
+            
             except Exception as e:
                 self.txtMain.clear()
                 self.txtMain.insertPlainText(str(e))
+            
             else:
+                self.closed = False
+
                 self.cmbPort.setEnabled(False)
-                self.btnOpen.setText('关闭串口')
+
+                if port == 'UDP Server':
+                    self.btnOpen.setText('停止监听')
+                elif port == 'UDP Client':
+                    self.btnOpen.setText('断开连接')
+                else:
+                    self.btnOpen.setText('关闭串口')
+
+                if port in ('UDP Server', 'UDP Client'):
+                    self.cmbIP.setEnabled(False)
+                    self.linPort.setEnabled(False)
+
         else:
+            self.closed = True
+
+            self.cmbPort.setEnabled(True)
+
+            if port == 'UDP Server':
+                self.btnOpen.setText('开启监听')
+            elif port == 'UDP Client':
+                self.btnOpen.setText('建立连接')
+            else:
+                self.btnOpen.setText('打开串口')
+
+            if port in ('UDP Server', 'UDP Client'):
+                    self.cmbIP.setEnabled(True)
+                    self.linPort.setEnabled(True)
+
             if self.rcvfile and not self.rcvfile.closed:
                 self.rcvfile.close()
             
-            self.ser.close()
-            
-            self.cmbPort.setEnabled(True)
-            self.btnOpen.setText('打开串口')
-    
-    @pyqtSlot()
-    def on_btnSend_clicked(self):
-        if self.ser.is_open:
-            text = self.txtSend.toPlainText()
-
-            if self.cmbOCode.currentText() == 'HEX':
+            if port in ('UDP Server', 'UDP Client'):
                 try:
-                    self.ser.write(bytes([int(x, 16) for x in text.split()]))   # for example, text = '55 AA 5A'
+                    self.sock.shutdown(socket.SHUT_RDWR)
+                except Exception as e:
+                    print(e)
+                
+                try:
+                    self.sock.close()
                 except Exception as e:
                     print(e)
 
             else:
-                text = text.replace('\n', self.cmbEnter.currentText().replace(r'\r', '\r').replace(r'\n', '\n'))
-                
+                self.ser.close()
+    
+    @pyqtSlot()
+    def on_btnSend_clicked(self):
+        if not self.closed:
+            text = self.txtSend.toPlainText()
+            try:
+                if self.cmbOCode.currentText() == 'HEX':
+                    bstr = bytes([int(x, 16) for x in text.split()])   # for example, text = '55 AA 5A'
+                else:
+                    text = text.replace('\n', self.cmbEnter.currentText().replace(r'\r', '\r').replace(r'\n', '\n'))
+                    bstr = text.encode(self.cmbOCode.currentText())
+            except Exception as e:
+                print(e)
+                return
+
+            port = self.cmbPort.currentText()
+            try:
+                if port == 'UDP Server':
+                    self.sock.sendto(bstr, self.clinAddr)
+                elif port == 'UDP Client':
+                    self.sock.sendto(bstr, (self.cmbIP.currentText(), int(self.linPort.text())))
+                else:
+                    self.ser.write(bstr)
+            except Exception as e:
+                print(e)
+    
+    def thread_sock_rcv(self):
+        while not self.exited:
+            port = self.cmbPort.currentText()
+            if not self.closed:
                 try:
-                    self.ser.write(text.encode(self.cmbOCode.currentText()))
+                    if port == 'UDP Server':
+                        rcvdbytes, self.clinAddr = self.sock.recvfrom(1024)
+                        self.rcvQueue.put(rcvdbytes)
+
+                    elif port == 'UDP Client':
+                        rcvdbytes, addr = self.sock.recvfrom(1024)
+                        self.rcvQueue.put(rcvdbytes)
+
                 except Exception as e:
                     print(e)
-    
-    def on_tmrSer_timeout(self):
-        self.tmrSer_Cnt += 1
 
-        if self.ser.is_open:
-            if self.ser.in_waiting > 0:
+            time.sleep(0.01)
+
+    def on_tmrRecv_timeout(self):
+        port = self.cmbPort.currentText()
+        if not self.closed:
+            if port in ('UDP Server', 'UDP Client'):
+                rcvdbytes = b'' if self.rcvQueue.empty() else self.rcvQueue.get()
+            else:
                 rcvdbytes = self.ser.read(self.ser.in_waiting)
 
+            if rcvdbytes:
                 if self.rcvfile and not self.rcvfile.closed:
                     self.rcvfile.write(rcvdbytes.decode('latin-1'))
                 
@@ -181,7 +274,7 @@ class SERCOM(QWidget):
                         
                         self.rcvbuff = self.rcvbuff[self.rcvbuff.rfind(b',')+1:]
 
-                        if self.tmrSer_Cnt % 4 == 0:
+                        if self.tmrRecv_Cnt % 4 == 0:
                             if len(d[-1]) != len(self.PlotChart.series()):
                                 for series in self.PlotChart.series():
                                     self.PlotChart.removeSeries(series)
@@ -261,32 +354,55 @@ class SERCOM(QWidget):
                     self.txtMain.moveCursor(QtGui.QTextCursor.End)
                     self.txtMain.insertPlainText(text)
 
-            if self.AutoInterval and self.tmrSer_Cnt % self.AutoInterval == 0:
+            if self.AutoInterval and self.tmrRecv_Cnt % self.AutoInterval == 0:
                 self.on_btnSend_clicked()
 
         else:
-            if self.tmrSer_Cnt % 100 == 0:
-                if len(comports()) != self.cmbPort.count():
+            if self.tmrRecv_Cnt % 100 == 0:
+                if len(comports()) + 2 != self.cmbPort.count():
                     self.cmbPort.clear()
                     for port, desc, hwid in comports():
                         self.cmbPort.addItem(f'{port} ({desc[:desc.index("(")]})')
+                    self.cmbPort.addItems(['UDP Client', 'UDP Server'])
 
-    @pyqtSlot(str)
-    def on_cmbAuto_currentIndexChanged(self, text):
-        if self.cmbAuto.currentText() == 'NO Auto':
-            self.AutoInterval = 0
+        self.tmrRecv_Cnt += 1
 
-        elif self.cmbAuto.currentText().endswith('s'):
-            self.AutoInterval = float(self.cmbAuto.currentText()[:-1]) * 100
-
-        elif self.cmbAuto.currentText().endswith('m'):
-            self.AutoInterval = float(self.cmbAuto.currentText()[:-1]) * 100 * 60
-    
     @pyqtSlot(int)
     def on_chkWave_stateChanged(self, state):
         self.ChartView.setVisible(state == Qt.Checked)
         self.txtMain.setVisible(state == Qt.Unchecked)
     
+    @pyqtSlot(str)
+    def on_cmbPort_currentIndexChanged(self, text):
+        if text in ('UDP Client', 'UDP Server'):
+            self.hWidget2.setVisible(False)
+            self.hLayout2.removeWidget(self.btnClear)
+            self.hLayout2.removeWidget(self.chkSave)
+
+            self.hLayout3.addWidget(self.btnClear)
+            self.hLayout3.addWidget(self.chkSave)
+            self.hWidget3.setVisible(True)
+
+            self.cmbBaud.setEnabled(False)
+
+            if 'Server' in text:
+                self.btnOpen.setText('开启监听')
+            else:
+                self.btnOpen.setText('建立连接')
+
+        else:
+            self.hWidget3.setVisible(False)
+            self.hLayout3.removeWidget(self.btnClear)
+            self.hLayout3.removeWidget(self.chkSave)
+
+            self.hLayout2.addWidget(self.btnClear)
+            self.hLayout2.addWidget(self.chkSave)
+            self.hWidget2.setVisible(True)
+
+            self.cmbBaud.setEnabled(True)
+
+            self.btnOpen.setText('打开串口')
+
     @pyqtSlot(str)
     def on_cmbBaud_currentIndexChanged(self, text):
         self.ser.baudrate = int(text)
@@ -303,6 +419,17 @@ class SERCOM(QWidget):
     def on_cmbStop_currentIndexChanged(self, text):
         self.ser.stopbits = int(text)
     
+    @pyqtSlot(str)
+    def on_cmbAuto_currentIndexChanged(self, text):
+        if text == 'NO Auto':
+            self.AutoInterval = 0
+
+        elif text.endswith('s'):
+            self.AutoInterval = float(text[:-1]) * 100
+
+        elif text.endswith('m'):
+            self.AutoInterval = float(text[:-1]) * 100 * 60
+
     @pyqtSlot()
     def on_btnClear_clicked(self):
         self.txtMain.clear()
@@ -310,6 +437,8 @@ class SERCOM(QWidget):
     def closeEvent(self, evt):
         if self.rcvfile and not self.rcvfile.closed:
             self.rcvfile.close()
+
+        self.exited = True
 
         self.ser.close()
 
@@ -319,6 +448,10 @@ class SERCOM(QWidget):
         self.conf.set('encode', 'output', self.cmbOCode.currentText())
         self.conf.set('encode', 'oenter', self.cmbEnter.currentText())
         self.conf.set('others', 'history', self.txtSend.toPlainText())
+
+        self.conf.set('network', 'ip', repr([self.cmbIP.currentText()] + [self.cmbIP.itemText(i) for i in range(self.cmbIP.count())]))
+        self.conf.set('network', 'port', self.linPort.text())
+
         self.conf.write(open('setting.ini', 'w', encoding='utf-8'))
 
 
